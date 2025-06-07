e2f759cabfef2867262b45be277413f86b4fac81
Nico Vece <me@nicovece.com>
2025-06-07 17:46:25 +0200

    Issue: changes build site referece

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://nicovece.github.io/cf-portfolio-astro/',
   integrations: [mdx(), sitemap()],
 });
