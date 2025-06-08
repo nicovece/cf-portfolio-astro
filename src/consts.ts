5409e67cccdfc3d2c3e54ec3ef1f3d368eab5c76
Nico Vece <me@nicovece.com>
2025-06-08 20:36:06 +0200

    Fix: fixed page title bug and fixed misspelling

--- src/consts.ts
+++ src/consts.ts
@@ -2,5 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE =
-  'Nico Vece - A brand new full (junior) stack developer';
-export const SITE_DESCRIPTION = 'A brand new full (junior) stack developer';
+  'Nico Vece - A brand new (junior) full stack developer';
+export const SITE_DESCRIPTION = 'A brand new (junior) full stack developer';
